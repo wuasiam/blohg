c64fb208e26745b86471e93573faea9e07ce4ef2
Rafael G. Martins <rafael@rafaelmartins.eng.br>
2012-10-18 21:28:48 -0300

    renamed plugins endpoint from blohg.plugins to blohg.ext

--- blohg/__init__.py
+++ blohg/__init__.py
@@ -76,9 +76,9 @@ class BlohgApp(Flask):
     def wsgi_app(self, *args, **kwargs):
         if not self.got_first_request and self.enable_plugins:
             self.hg.reload()
-            MercurialImporter.new(self, 'blohg.plugins')
+            MercurialImporter.new(self, 'blohg.ext')
             with self.app_context():
-                __import__('blohg.plugins')
+                __import__('blohg.ext')
                 ctx = _app_ctx_stack.top
                 if hasattr(ctx, 'plugin_registry'):
                     for plugin in ctx.plugin_registry:
